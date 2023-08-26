0d53c887cc96d87854353b63e91487a849740c40
tristan <tlea5@uw.com>
2023-08-25 17:26:35 -0700

    fixed a mobile issue

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+$(window).on('scroll', function() {
+    var scrolled = $(window).scrollTop();
+    $('.parallax-bg').css('top', -(scrolled * 0.3) + 'px');
+});
+
+//comments are previous attempts and ideas, keeping them for inspiration if needed
+
 // window.addEventListener("scroll", function(){
 //     var scrollTop = window.scrollY;
 //     document.body.style.backgroundPositionY = scrollTop * 0.8 + "px";
